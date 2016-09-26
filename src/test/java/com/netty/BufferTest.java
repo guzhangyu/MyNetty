b21f615a2f87504aea77b5c920b6f10c59c52512
zhangyu gu <guzhangyu@raycloud.com>
2016-09-26 12:54:36 +0800

    test code

---
+++ src/test/java/com/netty/BufferTest.java
@@ -0,0 +1,12 @@
+package com.netty;
+
+/**
+ * Created by guzy on 16/9/24.
+ */
+public class BufferTest {
+
+    public void testPoolBuffer(){
+        int loop=3000000;
+        long startTime=System.currentTimeMillis();
+    }
+}
